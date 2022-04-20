04cd15992f4f5c98a3d069ca2c4158ac353b9f9c
bellasoniadwi <bellasoniadwi@gmail.com>
2022-04-20 15:21:47 +0700

    add method PUT view edit

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
         <form action="/articles/{{ $article->id }}" method="post" enctype="multipart/form-data">
+            @method('PUT')
             @csrf
             <div class="form-group">
                 <label for="title">Judul</label>
@@ -16,7 +17,7 @@
             </div>
             <div class="form-group">
                 <label for="image">Feature Image</label>
-                <input type="file" class="form-control" required="required" name="image"
+                <input type="file" class="form-control" name="image"
                     value="{{ $article->featured_image }}"><br>
                 <img width="150px" src="{{ asset('storage/' . $article->featured_image) }}">
             </div>
